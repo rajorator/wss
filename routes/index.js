205cdb078cfdec3cf48c5c033d93e66983a035ae
datta <rajdatta@4c32759bb003.ant.amazon.com>
2017-07-24 18:06:39 -0700

    add new js files for support. add guages and date fields to start connecting

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,12 @@
 var express = require('express');
 var router = express.Router();
+var benis = require('../bin/benefits');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  console.log(benis.length);
+  res.render('index', { title: 'Express', benefits: benis });
 
-  
 });
 
 module.exports = router;
